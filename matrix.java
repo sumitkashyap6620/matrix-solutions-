58b0f3134edff921ca819cb8297b3aa776036646
sumitkashyap6620 <sumit.kashyap.6620@gmail.com>
2024-12-31 14:02:18 +0530

    add new line

--- matrix.java
+++ matrix.java
@@ -2,6 +2,7 @@
 
 public class matrix {
     public static void main(String[] args) {
-      System.out.println("welcome to my first git hub project :");  
+      System.out.println("welcome to my first git hub project :"); 
+      System.out.println("how are you sir : "); 
     }
 }
